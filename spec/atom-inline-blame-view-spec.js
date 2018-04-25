6b57f2426993b75cacb469f79842fe7adbbdd6fa
Aditya Mukherjee <aditya@adityamukherjee.com>
2018-04-25 12:35:13 +0530

    remove unnecessary files

--- spec/atom-inline-blame-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomInlineBlameView from '../lib/atom-inline-blame-view';
-
-describe('AtomInlineBlameView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
